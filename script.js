44788d3d0179c6e09b2c6d12ed1dd2d8eb38dbe9
Esteban <emgarcia1@miuandes.cl>
2024-03-14 21:32:30 -0300

    first_commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function displayInput() {
+  var input = document.getElementById("userInput").value;
+  var outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = "tu numero de telefono es : + " + input;
+}
